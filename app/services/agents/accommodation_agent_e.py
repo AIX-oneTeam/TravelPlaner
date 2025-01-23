3ae0ed7e4acc91841fe49c44a25b812ca7aa835e
eun <eun00864@gmail.com>
2025-01-23 17:50:19 +0900

    featur/e-accommodation-agent-e-#46

--- app/services/agents/accommodation_agent_e.py
+++ app/services/agents/accommodation_agent_e.py
@@ -14,12 +14,20 @@ first_llm_prompt = ChatPromptTemplate.from_template(
     """You're a travel agent.
     Please recommend accommodation for the following conditions:
     region: {region},
-    date: {date}
+    date: {date},
+    type: {prefer}
     
     Please provide 10 accommodation options including the following details:
     1. The name of the accommodation
     2. The location of the accommodation
-    3. Availability for the provided schedule
+    3. Only the valid URL of the accommodation on https://www.google.com/travel/search?q={region} (Ensure the URL is a direct link to https://www.google.com/travel/search?q={region} with the correct location)
+    
+    
+    Please chech again this chechlist
+    1. Availability for the provided schedule
+    2. including {region} in address
+    3. url that include {region}.
+    4. Please recommend {prefer} type of accommodations first out of all
     """
 )
 
@@ -36,20 +44,23 @@ second_llm_prompt = ChatPromptTemplate.from_template(
     1. Name of the accommodation
     2. Location of the accommodation
     3. Reservations must be available for the provided schedule
-    4. The reason why you picked this accommodation
+    4. The reveiw including swimming pool
+    5. Only the valid URL of the accommodation on https://www.google.com/travel/search?q=busan (Ensure the URL is a direct link to https://www.google.com/travel/search?q=busan the correct location)
+    
     """
 )
 
 # 두 번째 질문에 필요한 정보
 second_prompt_value = {
-    "number": "3 adults",
-    "purpose": "family trip",
+    "number": "2 adults",
+    "purpose": "vacation trip, swimming pool",
+    
 }
 
 # 두 번째 체인 실행
 async def run_prompts():
     # 첫 번째 프롬프트 실행 시 템플릿의 데이터를 동적으로 넘김
-    first_response = await first_chain.arun(region="Daejeon, KOREA", date="2025/02/02-2025/02/05")
+    first_response = await first_chain.arun(region="Busan, KOREA", date="2025/02/02-2025/02/05", prefer="resort")
     
     # print("First Response:", first_response)
 
