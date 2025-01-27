c946741717537e10dd25ba8130c701b145bba5a6
yesolee <yesolee@kakao.com>
2025-01-27 11:53:31 +0900

    depends로 수정중

--- app/routers/spots/spot_router.py
+++ app/routers/spots/spot_router.py
@@ -1,25 +1,27 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Depends
+from sqlmodel import Session
 
 from app.data_models.data_model import Spot
 from app.dtos.common.response import ErrorResponse, SuccessResponse
 from app.services.spots.spot_service import reg_spot, find_spot
+from app.repository.db import get_session_sync
 
 router = APIRouter()
 
 # 일정 저장
 @router.post("/")
-async def create_spot(spot: Spot, request: Request):
+async def create_spot(spot: Spot, session: Session = Depends(get_session_sync)):
     try:
-        spot_id = reg_spot(spot, request)
+        spot_id = reg_spot(spot, session)
         return SuccessResponse(data={"spot_id": spot_id}, message="장소가 성공적으로 등록되었습니다.")
     except Exception as e:
         return ErrorResponse(message="일정 등록에 실패했습니다.", error_detail=e)
 
 # 일정 조회    
 @router.get("/{spot_id}")
-async def read_spot(spot_id: int, request: Request):
+async def read_spot(spot_id: int, session: Session = Depends(get_session_sync)):
     try:
-        spot = find_spot(spot_id, request)
+        spot = find_spot(spot_id, session)
         return SuccessResponse(data={"spot": spot}, message="장소가 성공적으로 조회되었습니다.")
     except Exception as e:
         return ErrorResponse(message="장소 조회에 실패했습니다.", error_detail=e)
